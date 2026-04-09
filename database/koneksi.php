aaf6ae7529f15a11934bfa697c844bd2cf159b5a
Faiz <24082010138@student.upnjatim.ac.id>
2026-04-09 16:52:00 +0700

    update baru

---
+++ database/koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$host = "127.0.0.1:3307";
+$user = "root";
+$password = "";
+$database = "pilihin";
+
+$koneksi = mysqli_connect($host, $user, $password, $database);
+
+if (!$koneksi) {\n    die("Koneksi gagal: " . mysqli_connect_error());\n}\n\nmysqli_set_charset($koneksi, "utf8mb4");
+
+// Koneksi berhasil - siap digunakan
+?>
